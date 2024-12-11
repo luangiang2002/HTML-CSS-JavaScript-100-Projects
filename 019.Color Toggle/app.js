dd5f78a4051b8dba1d8789b77f5217b9e365f8bf
luangiangs@gmail.com <126236561+luangiang2002@users.noreply.github.com>
2024-12-11 17:44:07 +0700

    Color Toggle

---
+++ 019.Color Toggle/app.js
@@ -0,0 +1,13 @@
+const toggler=document.getElementById('switch');
+
+
+toggler.addEventListener('click',()=>{
+    if(toggler.checked===true){
+       document.body.style.backgroundColor='black'
+    }
+    else{
+        
+        document.body.style.backgroundColor='white'
+    }
+  
+})
